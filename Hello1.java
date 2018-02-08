f5cae9430a8efbf00c2a1de9cd6bde9e394b51a0
adheip <adheiplinux@gmail.com>
2018-02-08 20:01:16 +0000

    codes

---
+++ Hello1.java
@@ -0,0 +1,11 @@
+public class Hello1st{
+
+	public static void main(String[] args){
+
+		for(int i=1;i<=10;i++){
+	
+			System.out.println("Hello1... "+i);
+
+			}
+		}
+}
---
+++ Hello3.java
@@ -0,0 +1,12 @@
+public class Hello3rd{
+
+        public static void main(String[] args){
+
+                for(int i=1;i<=10;i++){
+
+                        System.out.println("Hello3... "+i);
+
+                        }
+                }
+}
+
